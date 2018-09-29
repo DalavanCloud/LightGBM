79e3bc82b76574982e9cd7cf1c2e07bcc31df576
Nikita Titov <nekit94-08@mail.ru>
2018-09-29 18:36:30 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -59,4 +59,4 @@ if [[ $TRAVIS == "true" ]]; then
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
 fi
-sudo apt-get install libc6=2.17-0ubuntu4
+sudo apt-cache madison libc6
